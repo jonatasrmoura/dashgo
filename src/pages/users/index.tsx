9f8ea920b10c6219d6dfabfad407b8bc0b2eca30
Jonatas Rosa Moura <jonatas.rmoura@outlook.com>
2022-09-09 15:56:46 -0300

    feat: Configurando React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import Link from "next/link";
 import {
   Box,
   Button,
@@ -5,6 +7,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -14,21 +17,26 @@ import {
   Tr,
   useBreakpointValue
 } from "@chakra-ui/react";
-import Link from "next/link";
-import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from '@tanstack/react-query';
+
 import { Header, Pagination, Sidebar } from "../../components";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery([`users`], async () => {
+    const response = await fetch(`http://localhost:3000/api/users`);
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
   useEffect(() => {
-    fetch(`http://localhost:3000/api/users`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+    
   }, []);
 
   return (
@@ -55,52 +63,64 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && <Th>Data de cadastro</Th> }
-                <Th w="8">
-                  <Box w="100%" display={[`none`, `flex`]}>Opções</Box>
-                </Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Jonatas Rosa Moura</Text>
-                    <Text fontSize="small" color="gray.300">
-                      jonatas.rmoura@outlook.com
-                    </Text>
-                  </Box>
-                </Td>
-                { isWideVersion && <Td>18 de Junho, 2022</Td> }
-                <Td>
-                  <Button
-                    as="a" /* Ancora */
-                    maxW={85}
-                    maxH={32}
-                    display={[`none`, `flex`]}
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th> }
+                    <Th w="8">
+                      <Box w="100%" display={[`none`, `flex`]}>Opções</Box>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Jonatas Rosa Moura</Text>
+                        <Text fontSize="small" color="gray.300">
+                          jonatas.rmoura@outlook.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    { isWideVersion && <Td>18 de Junho, 2022</Td> }
+                    <Td>
+                      <Button
+                        as="a" /* Ancora */
+                        maxW={85}
+                        maxH={32}
+                        display={[`none`, `flex`]}
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          ) }
         </Box>
       </Flex>
     </Box>
